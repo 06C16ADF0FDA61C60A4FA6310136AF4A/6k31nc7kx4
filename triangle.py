28527d237a800ccb6af0f22c20c9ee45c23c7737
06C16ADF0FDA61C60A4FA6310136AF4A <ccostellomail@gmail.com>
2017-04-13 16:58:22 +1000

    added iterate function

--- triangle.py
+++ triangle.py
@@ -1,15 +1,40 @@
+class Plex:
+    def __init__(self):
+        pass
+
 def tri(rows):
     data = []
     for r in range(rows):
         row = []
-        for n in range(rows-r): row+=[0]
+        for n in range(r+1): row+=[0]
         data+=[row]
     return data
 
-size = 8
-for line in tri(size):
-    emp = ''
-    for n in range(size-len(line)):
-        if n%2: emp+=' '
-        else: emp+='  '
-    print( emp+str(line) )
+
+def iterate(data,a,b):
+    while a+b<len(data):
+        for r in range(len(data)):
+            line = data[r]
+            #print( line, b )
+            line[b]=1-line[b]
+            if line[b]: a+=1
+            else: b+=1
+    return data
+                
+
+
+def display(data):
+    size = 8
+    for l in range(len(data)):
+        l = len(data)-l-1
+        line = data[l]
+        emp = ''
+        for n in range(size-len(line)):
+            if n%2: emp+=' '
+            else: emp+='  '
+        print( emp+str(line) )
+    print()
+
+triangle = tri(8)
+display(triangle)
+display(iterate(iterate(iterate(triangle,0,0),0,0),0,0))
